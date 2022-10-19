0698ed9dd88147ea708eda589e908c2efbd4235d
stefanadrian91 <110770697+stefanadrian91@users.noreply.github.com>
2022-10-19 17:32:09 +0300

    Update Dictionar.js

--- Dictionar.js
+++ Dictionar.js
@@ -1,17 +1,15 @@
 let numberOfWords = 1;
 let wordList = [];
 function addWord() {  												//function to add a word to the list
-	let word = $('#word').val();
-	$('#dictionary').append(`
-       	<li class="list-group-item" id="` + numberOfWords + `">` + word + `</li>
-	`);
-	wordList[numberOfWords] = $('#word').val();
+	let word = document.getElementById('word').value;
+	document.querySelector("#dictionary").innerHTML += ``+ word +` `+ " " + ``;
+	wordList[numberOfWords] = word;
 	++numberOfWords;
 	return false;
 }
 
 function findWord() {  												//function to find a word in the list
-	let wordToFind = $('#findWords').val();
+	let wordToFind = document.getElementById('findWords').value;
 	for (let i = 1; i < numberOfWords; ++i) {
 		if (wordList[i] === wordToFind) {
 			alert('The word already exists in the dictionary');
@@ -22,7 +20,7 @@ function findWord() {  												//function to find a word in the list
 
 function clearBox() {   											//function to clear the texbox after entering a word
 	event.preventDefault();
-    let wordClear = document.getElementById('word'); 
-    wordClear.value = '';
-    return false; 
+	let wordClear = document.getElementById('word'); 
+	wordClear.value = '';
+	return false; 
 }
